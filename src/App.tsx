4d287d3475ef1868e5ed8bb16fafb359a3082dc3
MD Nafizul Iqram <nafizulikram3@gmail.com>
2025-05-07 13:01:21 +0600

    Initialized project

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from "./components/ui/button"
+
+function App() {
+
+  return (
+    <>
+      <h1 className="text-7xl text-center"> <Button>ShadCN</Button></h1>
+    </>
+  )
+}
+
+export default App
