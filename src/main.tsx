3105bdfe9147d93f1ac2771063715951ee0b4415
MD Nafizul Iqram <nafizulikram3@gmail.com>
2025-05-13 19:58:12 +0600

    Fixed some errors

--- src/main.tsx
+++ src/main.tsx
@@ -8,6 +8,7 @@ import { Toaster } from 'sonner';
 import AuthProvider from './provider/AuthProvider.tsx';
 import PrivateRoute from './router/PrivetRoute.tsx';
 import { ThemeProvider } from './components/ui/theme/ThemeProvider.tsx';
+import { Analytics } from "@vercel/analytics/next"
 
 const router = createBrowserRouter([
   {
@@ -26,6 +27,7 @@ createRoot(document.getElementById('root')!).render(
       <ThemeProvider defaultTheme="light" storageKey="vite-ui-theme">
         <RouterProvider router={router} />
         <Toaster />
+        <Analytics/>
       </ThemeProvider>
     </AuthProvider>
   </StrictMode>,
